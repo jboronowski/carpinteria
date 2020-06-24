89449a3a40ff91c8a749e1bca18e9b69bddcf7c8
astrimuller <astri.muller@gmail.com>
2020-06-24 15:34:38 -0400

    deposito actualizado

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -6,10 +6,9 @@
     <a href="{{ route('items.index') }}"><i class="fa fa-edit"></i><span>Items</span></a>
 </li>
 
-<<<<<<< HEAD
+
 <li class="{{ Request::is('depositos*') ? 'active' : '' }}">
     <a href="{{ route('depositos.index') }}"><i class="fa fa-edit"></i><span>Depositos</span></a>
 </li>
 
-=======
->>>>>>> 08514be1eb3ca5370d4eced85853b568bb65bdc7
+
